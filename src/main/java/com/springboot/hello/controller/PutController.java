e788624ca99f47105042c74487aa149fafdbd250
Kimkiheon <cbj2741@gmail.com>
2022-10-27 14:41:49 +0900

    PutController ResponseEntity

--- src/main/java/com/springboot/hello/controller/PutController.java
+++ src/main/java/com/springboot/hello/controller/PutController.java
@@ -1,6 +1,8 @@
 package com.springboot.hello.controller;
 
 import com.springboot.hello.domain.dto.MemberDto;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.Map;
@@ -25,5 +27,9 @@ public class PutController {
     @PutMapping(value = "/member2")
     public MemberDto putMemberDto2(@RequestBody MemberDto memberDto){
         return memberDto;
+    }
+    @PutMapping(value = "/member3")
+    public ResponseEntity<MemberDto> postMemberDto3(@RequestBody MemberDto memberDto){
+        return ResponseEntity.status(HttpStatus.ACCEPTED).body(memberDto);
     }
 }
